b7b93bf77f80468e1511d932ef492a870fa1ee2a
Tozturk18 <tunaozturk2001@hotmail.com>
2024-12-19 18:37:33 +0300

    Added Scroll Animation to the main

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+function menuIcon() {
     const navMenuIcon = document.getElementById('navMenuIcon');
     const navLinks = document.getElementById('navLinks');
     const nav = document.querySelector('nav');
@@ -8,10 +8,31 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('show');
         navMenuIcon.classList.toggle('open');
     });
-});
+}
 
-window.onscroll = function() {
+function logoRoll() {
     const logo = document.getElementById('logo');
 
     logo.style.transform = "rotate(" + window.scrollY + "deg)";
+}
+
+function headAnimation() {
+    const carousel = document.querySelector('.carousel');
+    const head_text = document.getElementById('head_text');
+    const scroll_text = document.getElementById('scroll_text');
+
+    if (window.scrollY <= 200) {
+        carousel.style.transform = "translateY(" + window.scrollY*(6/3) + "px)";
+        head_text.style.transform = "translateY(" + -window.scrollY*2 + "px)";
+        scroll_text.style.opacity = window.scrollY/200;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', menuIcon);
+
+window.onscroll = function() {
+    
+    logoRoll();
+
+    headAnimation();
 };
