16c9bd43d406efe7f7151a7421f28331782220f7
Tozturk18 <tunaozturk2001@hotmail.com>
2024-12-19 19:43:13 +0300

    Updated the Scroll Animations

--- js/app.js
+++ js/app.js
@@ -18,12 +18,30 @@ function logoRoll() {
 
 function headAnimation() {
     const carousel = document.querySelector('.carousel');
+    const carousel_img = document.querySelector('.carousel_img');
+    const carousel_img_img = document.querySelectorAll('.carousel_img img');
     const head_text = document.getElementById('head_text');
     const scroll_text = document.getElementById('scroll_text');
 
     if (window.scrollY <= 200) {
-        carousel.style.transform = "translateY(" + window.scrollY*(6/3) + "px)";
+
+        if (window.innerWidth <= 768) {
+            carousel_img_img.forEach(img => {
+                img.style.width = 100 + (window.scrollY / 200 * 50) + "%";
+            });
+        } else {
+            carousel_img_img.forEach(img => {
+                img.style.width = 100 + (window.scrollY / 200 * 50 * 7/4) + "%";
+            });
+            carousel.style.margin = '0 '+ window.scrollY / 200 * 15 + '%';
+        }
+
+        carousel_img.style.opacity = 0.5 + (window.scrollY/200 * 0.5);
+        carousel.style.transform = "translateY(" + window.scrollY*2.25 + "px)";
+
+        head_text.style.opacity = 1 - (window.scrollY/200);
         head_text.style.transform = "translateY(" + -window.scrollY*2 + "px)";
+
         scroll_text.style.opacity = window.scrollY/200;
     }
 }
